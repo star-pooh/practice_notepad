ec69571c60a421d2a6e0374b7022ebfd42931014
star-pooh <eunki92@gmail.com>
2024-12-04 16:42:36 +0900

    init

---
+++ src/main/java/org/example/practice_notepad/PracticeNotepadApplication.java
@@ -0,0 +1,13 @@
+package org.example.practice_notepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeNotepadApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticeNotepadApplication.class, args);
+  }
+
+}
